1c7b1160c1e9cdf8014bf52fede45c1256aac662
ChisanuphongNuntta <tvrocker.www@gmail.com>
2021-08-11 11:40:24 +0700

    Heart

--- HelloCloud.py
+++ HelloCloud.py
@@ -1,20 +1,41 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Date
 
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://webadmin:SPYatb32373@node1247-rachpython.th.app.ruk-com.cloud:11001/testdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://webadmin:XCFbea12933@node17338-chisanuphong.app.ruk-com.cloud:11105/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Comments(db.Model):
+    __tablename__ = 'comments' 
+    name = Column(String,primary_key=True)
+    comment = Column(String)
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello Heart RUK-COM!</h1>'
+    result = Comments.query.all()
+    return render_template('index7.html', result=result)
 
 
-@app.route('/home',methods=['GET','POST'])
-def home():
-    links = ['https://ruk-com.in.th/',
-             'https://www.google.com/',
-             'https://www.python.org/',
-             'https://www.kmutnb.ac.th/']
-    return render_template('example.html',links=links)
+@app.route('/sign')
+def sign():
+    return render_template('sign7.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+    signature = Comments(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0',port=80)
+    app.run(debug=True)
